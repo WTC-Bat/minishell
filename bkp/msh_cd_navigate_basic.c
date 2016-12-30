76880e9ad4b2fbb71345387e868dd9ac3fa3a401
Matthew Williams <ElCapitan@Matthews-iMac.local>
2016-12-30 04:39:02 +0200

    began basic cd'ing

---
+++ bkp/msh_cd_navigate_basic.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+#include "minishell.h"
+
+/* EXAMPLES! COULD BE MERGED */
+
+void	cd_navigate_home(t_env *tenv)
+{
+	char	*home;
+	char	*pwd;
+
+	home = get_env_val(tenv, "HOME");
+	pwd = get_env_val(tenv, "PWD");
+	replace_var(tenv, "OLDPWD", pwd);
+	replace_var(tenv, "PWD", home);
+	ft_strdel(&pwd);
+	chsuccess = chdir(home);
+	ft_strdel(&home);
+	if (chsuccess != 0)
+		return (0);
+	return (1);
+}
+
+void	cd_navigate_root()
+{
+	char	*pwd;
+
+	pwd = get_env_val(tenv, "PWD");
+	replace_var(tenv, "OLDPWD", pwd);
+	replace_var(tenv, "PWD", "/");
+	chsuccess = chdir("/");
+	if (chsuccess != 0)
+		return (0);
+	return (1);
+}
--- src/msh_cd_navigate.c
+++ src/msh_cd_navigate.c
@@ -13,6 +13,45 @@
 #include "../libft/libft.h"
 #include "../includes/minishell.h"
 
+
+/* WIP [Still needs backward traversal and '-']*/
+static void cd_navigate_basic(char *arg)
+{
+	char	*pwd;
+	char	*new_pwd;
+	int		chsuccess;
+
+	pwd = get_env_val(tenv, "PWD");
+	chsuccess = 0;
+	if (arg == "/")
+		new_pwd = ft_strdup("/");
+	else if (arg == NULL || arg == "~")	//?
+		new_pwd = get_env_val(tenv, "HOME");
+	else if (arg == '-')
+		new_pwd = get_env_val(tenv, "OLDPWD");
+	chsuccess = chdir(new_pwd);
+	if (chsuccess == 0)
+	{
+		replace_var(tenv, "OLDPWD", pwd);
+		replace_var(tenv, "PWD", new_pwd);
+	}
+	else
+		ft_putendl_fd("ERROR: Unable to change directory.", 2);
+	// what if no if statements pass?
+	ft_strdel(&pwd);
+	ft_strdel(&new_pwd);
+}
+
+/*
+**	^
+**	|
+**	new
+**
+**	rework?
+**	|
+**	v
+*/
+
 static char	*get_path_back(char *arg, char *pwd)
 {
 	int		back_steps;
