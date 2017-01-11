ba7cb0eff8397b3c0afc64525e72d4ddde16d546
Matthew VAN WYK <mvanwyk@e5r1p7.jnb.42.fr>
2017-01-11 08:15:14 +0200

    all norm

--- includes/minishell.h
+++ includes/minishell.h
@@ -33,36 +33,24 @@ int					cd_navigate(char *arg, t_env *tenv);
 int					cd_navigate_basic(t_env *tenv, char *arg);
 int					cd_navigate_origin_home(t_env *tenv, char *arg);
 void				clear_star(char **args);
-
-void				cmds_free(t_list *cmds);					////
-
+void				cmds_free(t_list *cmds);
 void				free_star(char **star);
 void				free_tenv(t_env *tenv);
 t_env				*get_env();
 char				*get_env_val(t_env *tenv, char *varname);
-
-int					get_sc_split_count(char *input);			////
-
+int					get_sc_split_count(char *input);
 void				msh_cd(char **args, t_env *tenv);
-
-t_list				*msh_cmd_split(char *input);				////
-
+t_list				*msh_cmd_split(char *input);
 void				msh_echo(t_env *tenv, char **eargv);
 int					msh_exec(char **args, t_env *tenv);
-
-int					msh_handle_input(char **args, t_env *tenv);	////
-
+int					msh_handle_input(char **args, t_env *tenv);
 void				msh_setenv(t_env **tenv, char **args);
 void				msh_unsetenv(t_env **tenv, char **args);
-
-int					only_colon(char *input);					////
-
+int					only_colon(char *input);
 void				print_env(t_env *tenv);
 char				*read_line(const int fd);
 void				replace_var(t_env *tenv, char *var, char *val);
-
-char				**semicolon_split(char *input);				////
-
+char				**semicolon_split(char *input);
 int					strcnt(char **args);
 int					tenv_count(t_env *tenv);
 t_env				*t_env_reverse(t_env *tenv);
